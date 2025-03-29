5cc3a2123eb3920ddb8094c830daba75da29e012
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2025-03-29 17:45:03 +0530

    still not fixed

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -1,9 +1,13 @@
 import * as React from "react"
-import * as ToastPrimitives from "@radix-ui/react-toast"
+import * as ToastPrimitives from "@radix-ui/react-toast";
+// First install the package using:
+// npm install @radix-ui/react-toast
+// or
+// yarn add @radix-ui/react-toast
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 const ToastProvider = ToastPrimitives.Provider
 
